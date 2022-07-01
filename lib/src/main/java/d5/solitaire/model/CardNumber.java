83f962e03f66b7000937372453c45830269306cf
tomoyuki-nagata <tomolog925@gmail.com>
2022-07-01 20:24:55 +0900

    CardNumberをenumに変更

--- lib/src/main/java/d5/solitaire/model/CardNumber.java
+++ lib/src/main/java/d5/solitaire/model/CardNumber.java
@@ -1,29 +1,38 @@
 package d5.solitaire.model;
 
+import java.util.stream.Stream;
+
 import javax.annotation.Nonnull;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.ToString;
 
-@ToString
-@EqualsAndHashCode
-public class CardNumber {
-	private final Integer MIN_VALUE = 1;
-	private final Integer MAX_VALUE = 13;
+public enum CardNumber {
+
+	ONE(1, "A"),
+	TWO(2, "2"),
+	THREE(3, "3"),
+	FOUR(4, "4"),
+	FIVE(5, "5"),
+	SIX(6, "6"),
+	SEVEN(7, "7"),
+	EIGHT(8, "8"),
+	NINE(9, "9"),
+	TEM(10, "10"),
+	ELEVEN(11, "J"), 
+	TWELVE(12, "Q"),
+	THIRTEEN(13, "K");
 
 	@Getter
-	private Integer value;
+	private Integer number;
+	@Getter
+	private String label;
 
-	private CardNumber(@Nonnull Integer value) {
-		if (value < MIN_VALUE || MAX_VALUE < value) {
-			throw new IllegalArgumentException("無効な数字です。");
-		}
-		this.value = value;
+	private CardNumber(@Nonnull Integer number, @Nonnull String label) {
+		this.number = number;
+		this.label = label;
 	}
 
-	@Nonnull
-	public static CardNumber of(@Nonnull Integer value) {
-		return new CardNumber(value);
+	public static CardNumber of(@Nonnull Integer number) {
+		return Stream.of(values()).filter(numberLabel -> numberLabel.getNumber().equals(number)).findFirst().get();
 	}
 }
--- lib/src/test/java/d5/solitaire/model/CardNumberTest.java
+++ lib/src/test/java/d5/solitaire/model/CardNumberTest.java
@@ -3,6 +3,8 @@ package d5.solitaire.model;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.NoSuchElementException;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -15,19 +17,18 @@ class CardNumberTest {
 		CardNumber cardNumber = CardNumber.of(value);
 		assertAll(
 				() -> assertThat(cardNumber).isInstanceOf(CardNumber.class),
-				() -> assertThat(cardNumber.getValue()).isEqualTo(value)
+				() -> assertThat(cardNumber.getNumber()).isEqualTo(value)
 			);
 	}
 	
 	@ParameterizedTest
 	@ValueSource(ints = { 0, 14 })
 	void testファクトリメソッド_無効な数字(Integer value) {
-		var errorMessage = "無効な数字です。";
-		assertThrows(IllegalArgumentException.class, () -> CardNumber.of(value), errorMessage);
+		assertThrows(NoSuchElementException.class, () -> CardNumber.of(value));
 	}
 	
 	@Test
 	void testファクトリメソッド_null() {
-		assertThrows(NullPointerException.class, () -> CardNumber.of(null));
+		assertThrows(NoSuchElementException.class, () -> CardNumber.of(null));
 	}
 }
