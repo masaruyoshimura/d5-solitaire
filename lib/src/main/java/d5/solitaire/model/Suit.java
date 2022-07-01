e47779319bec174bc4206b4d702856a3155a0d24
tomoyuki-nagata <tomolog925@gmail.com>
2022-07-01 20:24:30 +0900

    lombokのGetterを利用

--- lib/src/main/java/d5/solitaire/model/Suit.java
+++ lib/src/main/java/d5/solitaire/model/Suit.java
@@ -2,23 +2,19 @@ package d5.solitaire.model;
 
 import javax.annotation.Nonnull;
 
+import lombok.Getter;
+
 public enum Suit {
 
 	SPADE("♠", "B"), CLUB("♣", "B"), HEART("♡", "R"), DIAMOND("♢", "R");
 
+	@Getter
 	private String label;
+	@Getter
 	private String color;
 
 	private Suit(@Nonnull String label, @Nonnull String color) {
 		this.label = label;
 		this.color = color;
-	}
-
-	public String label() {
-		return label;
-	}
-
-	public String color() {
-		return color;
 	}
 }
